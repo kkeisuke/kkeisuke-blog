99b4cb4249cf5993e547bb8619b84bd02ce4b4b6
kimura keisuke <mail@kkeisuke.com>
2020-12-25 19:01:24 +0900

    環境整備

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,22 @@ module.exports = {
   ],
   plugins: ['prettier'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'vue/array-bracket-spacing': 'error',
+    'vue/arrow-spacing': 'error',
+    'vue/block-spacing': 'error',
+    'vue/brace-style': 'error',
+    'vue/camelcase': 'error',
+    'vue/component-name-in-template-casing': 'error',
+    'vue/eqeqeq': 'error',
+    'vue/key-spacing': 'error',
+    'vue/match-component-file-name': 'error',
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/script-indent': 'error',
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': 'error',
+    'vue/v-on-function-call': 'error',
+    'no-console': 'warn',
+    'no-debugger': 'warn'
+  }
 }
